2ed2c88caa08e4fd7f71bbee036cf1b5313a4de9
sieging <sieging@gmail.com>
2017-10-30 10:02:08 +0900

    병합 테스트 pyc는 커밋 안해도 되나?

--- device/views.py
+++ device/views.py
@@ -8,14 +8,6 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-# def device_list(request):
-#     devices = BaseDevice.objects.all()
-#     param = {
-#         'title':"장치 리스트", 
-#         'devices':devices
-#         }
-#     return render(request, 'device_list.html', param)
-
 def device_list(request):
     devices_stb = Settopbox.objects.all()
     devices_ap = AccessPoint.objects.all()
